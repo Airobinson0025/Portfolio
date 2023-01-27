848443e6dec638d52e40597548658433bafb6519
Aaron Robinsom <airobinson0025@gmail.com>
2023-01-26 19:08:57 -0500

    Initial commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,6 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import logo from 'src/mylogo.png'
 import './globals.css'
 
 export default function RootLayout({ children }) {
@@ -8,7 +11,21 @@ export default function RootLayout({ children }) {
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div className='bg-white text-black font-bold flex flex-row justify-between items-center p-6'>
+          <div>
+            <Image src={logo} alt="Logo for portfolio" width={100} height={100}></Image>
+          </div>
+          <div className='flex flex-row space-x-4 items-center'>
+            <Link className='hover:text-red-500' href="/About">About</Link>
+            <Link className='hover:text-red-500' href="/Skills">Skills</Link>
+            <Link className='hover:text-red-500' href="/Projects">Projects</Link>
+            <Link className='hover:text-red-500' href="/Contact">Contact</Link>
+            <button className='bg-transparent hover:bg-black text-white-700 font-semibold hover:text-white py-2 px-4 border border-black hover:border-transparent rounded'>Resume.pdf</button>
+          </div> 
+        </div>
+        <div>{children}</div>
+        </body>
     </html>
   )
 }
