09f8fdcadfa183b8a5902e7724374bee3554e4fa
Aaron Robinsom <airobinson0025@gmail.com>
2023-02-07 10:37:27 -0500

    Second commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,5 @@
 import Link from 'next/link'
-import Image from 'next/image'
-import logo from 'src/mylogo.png'
-import Hero from './Hero'
+import Intro from './Intro'
 import './globals.css'
 
 export default function RootLayout({ children }) {
@@ -15,17 +13,17 @@ export default function RootLayout({ children }) {
       <body>
         <div className='bg-white text-black text-lg font-bold flex flex-row justify-between items-center p-6 max-w-7xl mx-auto'>
           <div>
-            <Image src={logo} alt="Logo for portfolio" width={120} height={120}></Image>
+            <p className='font-homemade text-3xl'>Aaron Robinson</p>
           </div>
           <div className='flex flex-row space-x-6 items-center'>
-            <Link className='hover:text-red-500' href="/About">About</Link>
-            <Link className='hover:text-red-500' href="/Skills">Skills</Link>
-            <Link className='hover:text-red-500' href="/Projects">Projects</Link>
-            <Link className='hover:text-red-500' href="/Contact">Contact</Link>
-            <button className='bg-transparent hover:bg-black hover:text-white font-bold py-2 px-4 border border-black hover:border-transparent rounded'>Resume.pdf</button>
+            <Link className='hover:text-red-500 transition duration-200' href="/About">About</Link>
+            <Link className='hover:text-red-500 transition duration-200' href="/Skills">Skills</Link>
+            <Link className='hover:text-red-500 transition duration-200' href="/Projects">Projects</Link>
+            <Link className='hover:text-red-500 transition duration-200' href="/Contact">Contact</Link>
+            <button className='bg-transparent hover:bg-black hover:text-white font-bold py-2 px-4 border border-black hover:border-transparent rounded transition duration-300'>Resume.pdf</button>
           </div> 
         </div>
-        <Hero />
+        <Intro />
         <div>{children}</div>
         </body>
     </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   "./components/**/*.{js,ts,jsx,tsx}",
 ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { homemade: ['Homemade Apple', 'cursive'] , 
+      space: [ 'Space Mono', 'monospace' ],
+      garamond: ['EB Garamond', 'serif'] }
+    },
   },
   plugins: [],
 }
